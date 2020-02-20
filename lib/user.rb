1068616121636e8bb0cd6b3cc9e012f85fe0bb07
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-20 07:02:57 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,10 @@
 class User 
+ 
   attr_accessor :firstname, :lastname
   attr_reader :firstname, :lastname
   
+  def initialize(firstname, lastname)
+    @firstname = firstname
+    @lastname = lastname
+  end 
 end 
